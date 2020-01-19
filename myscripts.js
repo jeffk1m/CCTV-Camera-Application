ec50461c285192e39d3e3acddf92a117eade748b
Bakguf <jeffreykim71@gmail.com>
2020-01-19 04:10:48 -0800

    description with a capital D

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,7 @@ function sendMail() {
     var link = "mailto:me@example.com"
              + "?cc=myCCaddress@example.com"
              + "&subject=" + escape("This is my subject")
-             + "&body=" + escape(document.getElementById('myText').value)
+             + "&body=" + escape(document.getElementById('Description').value)
     ;
 
     window.location.href = link;
